8428d67f047b3782340924def7b4f6105d58007e
Piyush Kachariya <pkachari@ford.com>
2022-02-27 18:39:30 -0600

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Piyush Kachariya on 2/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Piyush Kachariya on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
